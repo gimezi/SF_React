c1f939963f17d3a8b1010ec8862219a93a37e9c9
gimezi <yeji1107@naver.com>
2024-11-19 17:40:38 +0900

    [feat] next.js 프로젝트 진행중, css 깨짐이슈

--- next-board/components/ui/index.ts
+++ next-board/components/ui/index.ts
@@ -1,3 +1,23 @@
-export { Button, buttonVariants } from "@/components/ui/button";
-export { Input } from "@/components/ui/input";
-export { SearchBar } from "@/components/ui/serach-bar";
+export { Button, buttonVariants } from "./button/button";
+export { Calendar } from "./calendar/calendar";
+export { BasicDatePicker } from "./date-picker/date-picker";
+export { LabelDatePicker } from "./date-picker/label-date-picker";
+export { Input } from "./input/input";
+export { SearchBar } from "./search-bar/serach-bar"
+export { Progress } from "./progress/progress";
+export { Popover, PopoverTrigger, PopoverContent } from "./popover/popover";
+export {Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent} from '@/components/ui/card/card'
+export { Separator } from '@/components/ui/separator/separator'
+export { Checkbox } from '@/components/ui/checkbox/checkbox'
+export {
+    Dialog,
+    DialogPortal,
+    DialogOverlay,
+    DialogClose,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription
+} from '@/components/ui/dialog/dialog'
